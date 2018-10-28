f515c49c789fdd7aa065b526674a3eaf864bebae
Mikael <mkl.lrsn@gmail.com>
2018-10-28 13:13:05 +0100

    10

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,17 @@
-// 9
+// 10
 
-let aNumber = 2;
+function isEvenNumber(aNumber) {
+  return Number.isInteger(aNumber / 2);
+}
 
-console.log(Number.isInteger(aNumber / 2));
+let numbers = '';
+
+for(i = 1; i <= 10; i++) {
+  if(isEvenNumber(i)) {
+    numbers += i + '...';
+  } else {
+    numbers += i + '.';
+  }
+}
+
+console.log(numbers);
